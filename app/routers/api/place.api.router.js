53ca1db228427e6ed8f4b5f2257ec96317b57491
camillegripon <camille.gripon@oclock.school>
2024-07-10 17:17:57 +0200

    ending jsdoc

--- app/routers/api/place.api.router.js
+++ app/routers/api/place.api.router.js
@@ -1,12 +1,34 @@
 import express from 'express';
-import Controller from '../../controllers/api/place.api.controller.js' ;
+import Controller from '../../controllers/api/place.api.controller.js';
 
 const router = express.Router();
 
+/**
+ * Routeur pour les opérations de place.
+ * @module routers/place
+ */
 
+/**
+ * Route pour récupérer toutes les places et créer une nouvelle place.
+ * @name /
+ * @function
+ * @memberof module:routers/place
+ * @param {function} Controller.findAll - Contrôleur pour récupérer tous les lieux.
+ * @param {function} Controller.create - Contrôleur pour créer un nouveau lieu.
+ */
 router.route('/')
     .get(Controller.findAll.bind(Controller))
     .post(Controller.create.bind(Controller));
+
+/**
+ * Route pour récupérer, mettre à jour et supprimer un lieu via son ID.
+ * @name /:id
+ * @function
+ * @memberof module:routers/place
+ * @param {function} Controller.findById - Contrôleur pour récupérer un lieu via son ID.
+ * @param {function} Controller.update - Contrôleur pour mettre à jour un lieu via son ID.
+ * @param {function} Controller.delete - Contrôleur pour supprimer un lieu via son ID.
+ */
 router.route('/:id(\\d+)')
     .get(Controller.findById.bind(Controller))
     .patch(Controller.update.bind(Controller))
--- app/routers/api/register.api.router.js
+++ app/routers/api/register.api.router.js
@@ -3,10 +3,21 @@ import Controller from '../../controllers/api/user.api.controller.js';
 
 const router = express.Router();
 
+/**
+ * Routeur pour les opérations de l'utilisateur.
+ * @module routers/user
+ */
 
+/**
+ * Route pour inscrire un nouvel utilisateur et récupérer tous les utilisateurs.
+ * @name /register
+ * @function
+ * @memberof module:routers/user - le routeur découle du routeur principal
+ * @param {function} Controller.signUp - Contrôleur pour inscrire un nouvel utilisateur.
+ * @param {function} Controller.findAll - Contrôleur pour récupérer tous les utilisateurs.
+ */
 router.route('/')
     .post(Controller.signUp.bind(Controller))
-    .get(Controller.findAll.bind(Controller))
-
+    .get(Controller.findAll.bind(Controller));
 
 export default router;
