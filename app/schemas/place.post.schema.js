cfd0993ca865a687b6c9822168ce0e1a81c29c2a
marlu-mrtn <marlu.mrtn@gmail.com>
2024-07-11 13:52:13 +0200

    place.post schema

---
+++ app/schemas/place.post.schema.js
@@ -0,0 +1,19 @@
+import Joi from 'joi';
+
+/**
+ * Schéma de validation pour les places.
+ * @type {Joi.ObjectSchema}
+ * @property {Joi.StringSchema} name - Le nom de la place, doit être une chaîne de caractères avec un minimum de 2 caractères.
+ * @property {Joi.NumberSchema} gps_location - La position GPS de la place, doit être un nombre.
+ * @property {Joi.StringSchema} image - L'URL de l'image de la place, doit être une chaîne de caractères.
+ * @property {Joi.StringSchema} journey - La description du voyage associé à la place, doit être une chaîne de caractères.
+ * @property {Joi.StringSchema} description - La description de la place, doit être une chaîne de caractères.
+ */
+export default Joi.object({
+  name: Joi.string().min(2),
+  gps_location: Joi.number(),
+  image: Joi.array().items(Joi.string()),
+  journey: Joi.array().items(Joi.string()),
+  description: Joi.string(),
+  user_id: Joi.number(),
+});
