d4b0252fa8e8ee15f953307125115050df9ddaac
camillegripon <camille.gripon@oclock.school>
2024-07-13 16:09:22 +0200

    change sqitch for gps_location

--- app/schemas/place.post.schema.js
+++ app/schemas/place.post.schema.js
@@ -4,14 +4,16 @@ import Joi from 'joi';
  * Schéma de validation pour les places.
  * @type {Joi.ObjectSchema}
  * @property {Joi.StringSchema} name - Le nom de la place, doit être une chaîne de caractères avec un minimum de 2 caractères.
- * @property {Joi.NumberSchema} gps_location - La position GPS de la place, doit être un nombre.
+ * @property {Joi.NumberSchema} gps_location_latitude - La position GPS de la place, doit être un nombre.
+ * @property {Joi.NumberSchema} gps_location_longitude - La position GPS de la place, doit être un nombre.
  * @property {Joi.StringSchema} image - L'URL de l'image de la place, doit être une chaîne de caractères.
  * @property {Joi.StringSchema} journey - La description du voyage associé à la place, doit être une chaîne de caractères.
  * @property {Joi.StringSchema} description - La description de la place, doit être une chaîne de caractères.
  */
 export default Joi.object({
   name: Joi.string().min(2),
-  gps_location: Joi.number(),
+  gps_location_latitude: Joi.number().precision(8),
+  gps_location_longitude: Joi.number().precision(8),
   picture: Joi.array().items(Joi.string()),
   journey: Joi.array().items(Joi.string()),
   description: Joi.string(),
