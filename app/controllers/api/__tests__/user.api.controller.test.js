78167e2ef0cfc5398fdfde189b3277c7b5b94f5f
camillegripon <camille.gripon@oclock.school>
2024-07-15 16:28:41 +0200

    i hate jest

--- app/controllers/api/__tests__/user.api.controller.test.js
+++ app/controllers/api/__tests__/user.api.controller.test.js
@@ -1,71 +1,79 @@
 import { jest } from '@jest/globals';
 import controller from '../user.api.controller.js';
 
-const mockReq = {};
-const mockRes = {};
-mockRes.status = jest.fn().mockReturnThis();
-mockRes.send = jest.fn().mockReturnThis(); // Modifiez ceci pour retourner `this`
-mockRes.json = jest.fn().mockReturnThis(); // Assurez-vous de retourner `this` pour chaîner les méthodes
-const mockNext = jest.fn();
+let mockReq;
+let mockRes;
+let mockNext;
+
+beforeEach(() => {
+    // Initialiser les mocks avant chaque test
+    mockReq = {};
+    mockRes = {
+        status: jest.fn().mockReturnThis(),
+        send: jest.fn().mockReturnThis(),
+        json: jest.fn().mockReturnThis(),
+    };
+    mockNext = jest.fn();
+});
 
 describe('Controller methods', () => {
     let createdUserId;
 
     describe('findAll', () => {
-        beforeAll(async () => {
+        beforeEach(async () => {
             await controller.findAll(mockReq, mockRes, mockNext);
         });
-        
+
         test('res.json called', () => {
             expect(mockRes.json).toHaveBeenCalled();
         });
-        
+
         test('findAll method should call res.json with an object with arguments', () => {
             expect(mockRes.json).toHaveBeenCalledWith({ data: expect.any(Object) });
         });
     });
 
     describe('findById', () => {
-        const mockReq = { params: { id: 1 } };
-
-        beforeAll(async () => {
+        beforeEach(async () => {
+            mockReq = { params: { id: 1 } };
             await controller.findById(mockReq, mockRes, mockNext);
         });
-    
+
         test('res.json called', () => {
             expect(mockRes.json).toHaveBeenCalled();
         });
-    
+
         test('findById method should call res.json with an object with arguments', () => {
             expect(mockRes.json).toHaveBeenCalledWith({ data: expect.any(Object) });
         });
     });
 
     describe('create', () => { 
-        const mockReq = { body: { username: `harrypotter212}`, email: `harrypotter212@gmail.com`, password: "#1gKiJItKHLNp2"} };
-
-        beforeAll(async () => {
+        beforeEach(async () => {
+            // Assurez-vous que l'utilisateur n'existe pas avant de le créer
+            mockReq = { body: { username: `uniqueuser`, email: `uniqueuser@gmail.com`, password: "#1gKiJItKHLNp2" } };
             await controller.create(mockReq, mockRes, mockNext);
-            createdUserId = mockRes.json.mock.calls[0][0].data[0].id; // Assuming the created user ID is in the response data
+            if (mockRes.json.mock.calls.length > 0) {
+                createdUserId = mockRes.json.mock.calls[0][0].data.id;
+            }
         });
 
         test('res.status called', () => {
             expect(mockRes.status).toHaveBeenCalledWith(201);
         });
-    
+
         test('res.json called', () => {
             expect(mockRes.json).toHaveBeenCalled();
         });
-    
+
         test('create method should call res.json with an object with arguments', () => {
             expect(mockRes.json).toHaveBeenCalledWith({ data: expect.any(Object) });
         });
     });
 
     describe('delete', () => {
-        const mockReq = { params: { id: createdUserId } };
-
-        beforeAll(async () => {
+        beforeEach(async () => {
+            mockReq = { params: { id: createdUserId } };
             await controller.delete(mockReq, mockRes, mockNext);
         });
 
@@ -78,7 +86,7 @@ describe('Controller methods', () => {
         });
 
         test('delete method should call res.send with no arguments', () => {
-            expect(mockRes.send).toHaveBeenCalledWith(); // Attendez-vous à aucun argument
+            expect(mockRes.send).toHaveBeenCalledWith();
         });
     });
 });
