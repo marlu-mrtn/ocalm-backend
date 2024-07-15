8d2d25dc941b3ebbb543f77f8a66d233dc29e463
camillegripon <camille.gripon@oclock.school>
2024-07-15 15:16:23 +0200

    i hate jest

--- app/controllers/api/__tests__/user.api.controller.test.js
+++ app/controllers/api/__tests__/user.api.controller.test.js
@@ -1,14 +1,15 @@
 import { jest } from '@jest/globals';
-
 import controller from '../user.api.controller.js';
 
 const mockReq = {};
 const mockRes = {};
-mockRes.json = jest.fn(() => mockRes);
+mockRes.status = jest.fn().mockReturnThis();
+mockRes.send = jest.fn().mockReturnThis(); // Modifiez ceci pour retourner `this`
+mockRes.json = jest.fn().mockReturnThis(); // Assurez-vous de retourner `this` pour chaîner les méthodes
 const mockNext = jest.fn();
 
 describe('Controller methods', () => {
-    
+    let createdUserId;
 
     describe('findAll', () => {
         beforeAll(async () => {
@@ -41,10 +42,15 @@ describe('Controller methods', () => {
     });
 
     describe('create', () => { 
-        const mockReq = { body: { username: "harry", email: "harry@gmail.com", password: "#1gKiJItKHLNp2"} };
+        const mockReq = { body: { username: `harrypotter212}`, email: `harrypotter212@gmail.com`, password: "#1gKiJItKHLNp2"} };
 
         beforeAll(async () => {
             await controller.create(mockReq, mockRes, mockNext);
+            createdUserId = mockRes.json.mock.calls[0][0].data[0].id; // Assuming the created user ID is in the response data
+        });
+
+        test('res.status called', () => {
+            expect(mockRes.status).toHaveBeenCalledWith(201);
         });
     
         test('res.json called', () => {
@@ -56,4 +62,23 @@ describe('Controller methods', () => {
         });
     });
 
+    describe('delete', () => {
+        const mockReq = { params: { id: createdUserId } };
+
+        beforeAll(async () => {
+            await controller.delete(mockReq, mockRes, mockNext);
+        });
+
+        test('res.status called', () => {
+            expect(mockRes.status).toHaveBeenCalledWith(204);
+        });
+
+        test('res.send called', () => {
+            expect(mockRes.send).toHaveBeenCalled();
+        });
+
+        test('delete method should call res.send with no arguments', () => {
+            expect(mockRes.send).toHaveBeenCalledWith(); // Attendez-vous à aucun argument
+        });
+    });
 });
