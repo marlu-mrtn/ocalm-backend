d123bac21ce180a7bb2d36e09a3cf2eaf644c63d
camillegripon <camille.gripon@oclock.school>
2024-07-17 10:21:19 +0200

    we try to correct token

--- app/helpers/jwt.helpers.js
+++ app/helpers/jwt.helpers.js
@@ -1,24 +1,26 @@
 import jwt from 'jsonwebtoken';
+import ApiError from './api.error.js';
 
 export default {
-    
     generateToken: (user) => {
         return jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET, {
             expiresIn: '1h'
         });
     },
 
-     verifyToken: (bearer) => {
-        if (!bearer) {
+    verifyToken: (authHeader) => {
+        if (!authHeader) {
             throw new ApiError(401, 'Token absent');
         }
 
-        const [, token] = bearer.split(' ');
+        const bearer = authHeader.split(' ');
 
-        if (!token) {
+        if (bearer.length !== 2 || bearer[0] !== 'Bearer') {
             throw new ApiError(401, 'Token invalide');
         }
-    
+
+        const token = bearer[1];
+
         try {
             return jwt.verify(token, process.env.JWT_SECRET);
         } catch (error) {
