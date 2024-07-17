b45d5cba1f8f22c3db83ebdf4a6373f82fbf7742
camillegripon <camille.gripon@oclock.school>
2024-07-17 09:04:54 +0000

    jwt fixed

--- app/helpers/jwt.helpers.js
+++ app/helpers/jwt.helpers.js
@@ -9,20 +9,24 @@ export default {
     },
 
     verifyToken: (authHeader) => {
-        if (!authHeader) {
+	console.log("coucou", authHeader.headers.authorization);
+	const token = authHeader.headers.authorization;
+	console.log("ICI LE TOKEN ICIIIIIIIIIIIII", token);
+        if (!token) {
             throw new ApiError(401, 'Token absent');
         }
 
-        const bearer = authHeader.split(' ');
+        const bearer = token.split(' ');
 
         if (bearer.length !== 2 || bearer[0] !== 'Bearer') {
             throw new ApiError(401, 'Token invalide');
         }
 
-        const token = bearer[1];
+        const token1 = bearer[1];
+	console.log(token1);
 
         try {
-            return jwt.verify(token, process.env.JWT_SECRET);
+            return jwt.verify(token1, process.env.JWT_SECRET);
         } catch (error) {
             throw new ApiError(401, 'Token invalide');
         }
